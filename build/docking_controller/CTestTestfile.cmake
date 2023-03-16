8ffeb3cb8f499a13b2c6d6e9756b4740225c2693
Ubuntu <hadrienroy@gmail.com>
2023-03-16 08:27:16 +0000

    Delete colcon files

--- build/docking_controller/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/colcon_ws/src/my_docking_control/docking_controller
-# Build directory: /home/ubuntu/colcon_ws/src/build/docking_controller
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
